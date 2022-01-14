b6345c7a8220a318317c4b3e25f587d8cc22b03d
javekk <periniraffaele@gmail.com>
2022-01-15 00:30:19 +0100

    Logistic regression train

--- logisticRegression/logisticRegression.py
+++ logisticRegression/logisticRegression.py
@@ -1,6 +1,6 @@
 import numpy as np
 import sys
-
+import csv
 
 def sigmoid(z): 
     return 1 / (1 + np.exp(-z)) - 0.00000001
@@ -12,7 +12,9 @@ def predict(X, weights):
 def cost(X, y, weights):
     y_pred = predict(X, weights)
     m = len(X)
-    return -(1 / m) * np.sum(y*np.log(y_pred) + (1-y)*np.log(1-y_pred))
+    class_cost_y1 = y * np.log(y_pred)
+    class_cost_y0 = (1 - y) * np.log(1 - y_pred)
+    return -(1 / m) * np.sum(class_cost_y1 + class_cost_y0)
 
 def update_weights(X, y, weights, learning_rate):
     m = len(X)
@@ -23,27 +25,45 @@ def update_weights(X, y, weights, learning_rate):
     weights -= gradient
     return weights
 
-def train(X, y, weights, learning_rate, noEpoch):
+def train(X, y, weights, learning_rate, epochs):
     J = [cost(X, y, weights)] 
-    for i in range(0, noEpoch):
+    for i in range(0, epochs):
         weights = update_weights(X, y, weights, learning_rate)
         J.append(cost(X, y, weights))
+        if i % 1000 == 0:
+            print("Epoch: ", i, ", Cost: ", J[i])
     return J, weights
 
-def predict2(X, y, weights, learning_rate, epochs):
-    J, weights = train(X, y, weights, learning_rate, epochs) 
+def read_data(filePath):
+    with open(filePath, 'r') as f:
+        reader = csv.reader(f, delimiter=',')
+        headers = next(reader, None)
+        data = np.array(list(reader)).astype(float)
+        return data
+
+def data_preprocessing(data):
+    data = data[:, 1:] #remove id
+    y = np.where(data[:,9] == 4 , 1, 0) # benign(2) -> 0, malignant(4) -> 1
+    data = data[:, :-1] #remove y
+    return data, y
+
+def main():
+    if len(sys.argv) != 2:
+        print('Please specify (only) one argument i.e. the Breast Cancer Wisconsin csv file')
+        sys.exit()
+
+    X, y = data_preprocessing(read_data(sys.argv[1]))
+
+    weights = np.random.rand(len(X[0]))
+    learing_rate = 0.0001
+    epochs = 100000
+
+    J, weights = train(X, y, weights, learing_rate, epochs) 
     h = predict(X, weights)
     for i in range(len(h)):
         h[i] = 1 if h[i] >=0.5 else 0
     y = list(y)
     acc = np.sum([y[i] == h[i] for i in range(len(y))])/len(y)
-    return J, acc
-
-
-def main():
-    if len(sys.argv) != 2:
-        print('Please specify (only) one argument i.e. the csv file')
-        sys.exit()
 
 
 if __name__ == "__main__":
